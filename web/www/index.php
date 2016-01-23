e70bdd1c6ec3c6382dcccbe9982bbe11c805bfda
MestreLion <linux@rodrigosilva.com>
2016-01-23 18:52:20 -0200

    add index page

---
+++ web/www/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="shortcut icon" href="favicon.ico">
+	<title>MestreLion ConquerClub Tools</title>
+</head>
+<body>
+	<h2>Welcome to MestreLion's amazing ConquerClub humble tools!</h2>
+	<a href="cgi-bin/clanplayerrank">CL7 PACK Player Rank</a><br>
+	<br>
+	<a href="cgi-bin/league_scoreboard">CL7 Scoreboards</a><br>
+	<br>
+	<a href="cgi-bin/maprank">Map Rank</a> (soon!)<br>
+</body>
+</html>
