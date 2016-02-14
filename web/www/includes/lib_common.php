deeb1ca99a31718925e277fff4bdeffcb6b74f78
MestreLion <linux@rodrigosilva.com>
2016-02-14 03:53:07 -0200

    add common PHP functions library

---
+++ web/www/includes/lib_common.php
@@ -0,0 +1,14 @@
+<?php
+function get_str($var, $default="") {
+	return isset($_GET[$var]) ? $_GET[$var] : $default;
+}
+
+function get_int($var, $default=0) {
+	return intval(!empty($_GET[$var]) ? $_GET[$var] : $default);
+}
+
+# Alias to get_str
+function get($var, $default="") {
+	return get_str($var, $default);
+}
+?>
