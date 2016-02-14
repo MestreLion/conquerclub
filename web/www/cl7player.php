a2424abef3c6d465de4e45e0ea90eac74b46e40a
MestreLion <linux@rodrigosilva.com>
2016-02-14 10:32:25 -0200

    add CL7 Player Rank page

---
+++ web/www/cl7player.php
@@ -0,0 +1,9 @@
+<?include "globals.php"?>
+<?include "includes/lib_common.php"?>
+<?include "includes/lib_html.php"?>
+<?php
+HTML_Header("CL7 Player Rank");
+HTML_Content_Title("CL7 Player Rank", "");
+HTML_AnimatedLoadURL("cgi-bin/clanplayerrank.cgi");
+HTML_Footer();
+?>
