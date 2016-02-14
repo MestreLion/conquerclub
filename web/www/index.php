38f93d0eab0d63cf64880d02fe48c315bf4be40d
MestreLion <linux@rodrigosilva.com>
2016-02-14 09:49:24 -0200

    use the HTML library in the index page

--- web/www/index.php
+++ web/www/index.php
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<link rel="shortcut icon" href="favicon.ico">
-	<title>MestreLion ConquerClub Tools</title>
-</head>
-<body>
-	<h2>Welcome to MestreLion's amazing ConquerClub humble tools!</h2>
-	<a href="cgi-bin/clanplayerrank">CL7 PACK Player Rank</a><br>
-	<br>
-	<a href="cgi-bin/league_scoreboard">CL7 Scoreboards</a><br>
-	<br>
-	<a href="cgi-bin/maprank">Map Rank</a> (soon!)<br>
-	<br>
-	<a href="template.html">HTML Template</a><br>
-	<br>
-	<a href="template.php">PHP Template</a><br>
-	<br>
-	<a href="phpinfo.php">PHP Information</a><br>
-</body>
-</html>
+<?include "globals.php"?>
+<?include "includes/lib_common.php"?>
+<?include "includes/lib_html.php"?>
+<?php
+HTML_Header();
+HTML_Content_Title("Main Page", "Use the menu on the left to access the tools");
+HTML_Footer();
+?>
