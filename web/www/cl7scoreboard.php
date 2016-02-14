136c86d66d63fc8bbcfe1a69f5254064c3259bc0
MestreLion <linux@rodrigosilva.com>
2016-02-14 10:21:23 -0200

    use animated AJAX loader to load CGI content

--- web/www/cl7scoreboard.php
+++ web/www/cl7scoreboard.php
@@ -5,11 +5,5 @@
 HTML_Header("CL7 Scoreboards");
 HTML_Content_Title("CL7 Scoreboards", "");
 HTML_AnimatedLoadURL("cgi-bin/league_scoreboard.cgi");
-#virtual("cgi-bin/league_scoreboard.cgi");
-#HTML_Content();
 HTML_Footer();
-
 ?>
-<?function HTML_Content() {?>
-<!--#include virtual="cgi-bin/league_scoreboard.cgi" -->
-<?}?>
