5960e5dda6662d9351f67736e222880fb1180166
MestreLion <linux@rodrigosilva.com>
2016-02-14 04:53:38 -0200

    split html template into PHP functions

---
+++ web/www/includes/lib_html.php
@@ -0,0 +1,204 @@
+<?php
+function HTML_Header() {
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>MestreLion's ConquerClub Tools</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="description" content="Tools for ConquerClub players" />
+	<meta name="keywords" content="conquer club,risk" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="stylesheet" href="css/skidoo_too.min.css" type="text/css" />
+	<link rel="stylesheet" href="css/stylesheet.css" type="text/css" />
+	<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+	<script type="text/javascript" src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+	<link   type="text/css"       href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.min.css" rel="stylesheet" />
+	<script type="text/javascript">
+		// Load local jquery as fallback if CDN is not available
+		// Use a single test for all files, assuming that if first fails all others will fail too
+		window.jQuery ||
+		document.write(
+			'\x3Cscript type="text/javascript" src="js/jquery-1.12.0.min.js"\x3E\x3C/script\x3E\n' +
+			'\x3Cscript type="text/javascript" src="js/jquery-ui-1.11.4.min.js"\x3E\x3C/script\x3E\n' +
+			'\x3Clink   type="text/css"      href="css/jquery-ui-1.11.4.smoothness.min.css" rel="stylesheet" /\x3E\n'
+		);
+	</script>
+	<script type="text/javascript">
+		var app_id = '';
+		var app_context = '';
+		var is_mobile_device = false;
+		var is_new_recruit = false;
+		var is_new_player = false;
+		var currenttime = 'Jan 31 2016, 18:46:31';
+		var GameHideNav = 'N';
+	</script>
+	<script type="text/javascript" src="js/common.js"></script>
+	<script type="text/javascript" src="js/lightbox.js"></script>
+	<script type="text/javascript" src="js/pagination1.js"></script>
+</head>
+
+<body>
+<div id="pageWrapper">
+
+<div id="mainheader">
+	<div id="mainNavAndLogo">
+		<div id=CC_mainlogo><a href="template.html"><img src="images/static/cc_logo.png" alt="MestreLion's Conquer Club Tools" title="MestreLion's ConquerClub Tools" /></a></div>
+		<div id=mainNav>
+			<li><a>Welcome to MestreLion's amazing ConquerClub humble tools!</a></li>
+		</div>
+	</div>
+	<div id="rightNav">
+		<li><a href="">FORUM</a></li><br />
+		<li><a href="">HELP</a></li><br />
+		<li><a href="">RULES</a></li><br />
+	</div>
+</div>
+<div id="mainheader_mobile" >
+	<div id=CC_mainlogo_mobile><a href=""><img src="images/static/icon.png" alt="MestreLion's Conquer Club Tools" title="MestreLion's ConquerClub Tools" /></a></div>
+	<div id=mainNav_mobile>
+		<span><a>MestreLion's ConquerClub Tools</a></span>
+	</div>
+</div>
+
+<div id="outerColumnContainer">
+	<div id="innerColumnContainer">
+		<div id="SOWrap">
+			<div id="leftColumn">
+				<div id=leftColumninner class="inside">
+<!--- left column begin -->
+<div class="vnav" id="leftnav">
+	<div id="cctime" style="font-size:12px; font-weight:bold; color:black; display: inline-block; text-align:center;z-index:10;padding:0px;width:100%;">
+		<span id="servertime">Jan 31, 18:46:31 CCT</span>
+	</div>
+	<p style='margin-bottom:0px;width:100%;display: inline-block; text-align:center;'>[<a href="/conquerclub">logout <b>MestreLion</b></a>]</p>
+
+	<h3>Game Menu</h3>
+	<ul>
+		<li><a href="">Central Command</a></li>
+		<li><a href="">Join Games</a></li>
+		<li><a href="">Start Games</a></li>
+		<li><a href="">Game Finder</a></li>
+		<li><a href="">Battle Royale</a></li>
+		<li><a href="">Other Games</a></li>
+	</ul>
+
+	<h3>Announcements</h3>
+	<ul style='opacity:.9;font-size:10px;'>
+		<li><a title='Clans: February Platoon Report is here!' href=''><span class='player6'>&bull;</span>February Platoon Report is here!</a></li>
+		<li><a title='Events: Official February Challenge -- Febenario!' href=''><span class='player5'>&bull;</span>Official February Challenge -- Febenario!</a></li>
+		<li><a title='Tournaments: Fight For a Beer' href=''><span class='player7'>&bull;</span>Fight For a Beer</a></li>
+		<li class=announcements style='display:none;'><a title='Events: Monumental Battle Royale Game Reforged' href=''><span class='player5'>&bull;</span>Monumental Battle Royale Game Reforged</a></li>
+		<li class=announcements style='display:none;'><a title='Community: Congrats to GreenBaize on SOS Africa II Triumph' href=''><span class='player2'>&bull;</span>Congrats to GreenBaize on SOS Africa II Triumph</a></li>
+		<li class=announcements style='display:none;'><a title='Events: Five tourneys left for Decade in Review Challenge' href=''><span class='player5'>&bull;</span>Five tourneys left for Decade in Review Challenge</a></li>
+		<li class=announcements style='display:none;'><a title='Tournaments: Quads Teams Needed for New TPA tourney.' href=''><span class='player7'>&bull;</span>Quads Teams Needed for New TPA tourney.</a></li>
+		<li class=announcements style='display:none;'><a title='Community: Say Something Nice' href=''><span class='player2'>&bull;</span>Say Something Nice</a></li>
+		<li class=announcements style='display:none;'><a title='Community: Congratulations to all the Olympic Medalists' href=''><span class='player2'>&bull;</span>Congratulations to all the Olympic Medalists</a></li>
+		<li class=announcements style='display:none;'><a title='Webmaster: CC Will Soon Celebrate its Tenth Birthday!' href=''><span class='player1'>&bull;</span>CC Will Soon Celebrate its Tenth Birthday!</a></li>
+		<li class=announcements style='display:none;'><a title='Maps: WWI Gallipoli has been Quenched!' href=''><span class='player3'>&bull;</span>WWI Gallipoli has been Quenched!</a></li>
+		<li class=announcements style='display:none;'><a title='Maps: Beta testers needed for new maps.' href=''><span class='player3'>&bull;</span>Beta testers needed for new maps.</a></li>
+		<li class=announcements style='display:none;'><a title='Webmaster: New Campaigns!' href=''><span class='player1'>&bull;</span>New Campaigns!</a></li>
+		<li class=announcements style='display:none;'><a title='Maps: Open callout for Foundry Volunteers' href=''><span class='player3'>&bull;</span>Open callout for Foundry Volunteers</a></li>
+		<li><a id=more href="#" onClick="showannouncements(); return false;">More</a></li>
+	</ul>
+
+	<h3>Interaction Menu</h3>
+	<ul>
+		<li><a href="">Forum</a></li>
+		<li><a href="">Chat Rooms</a></li>
+		<li><a href="">Inbox<span id="privmsg"></span></a></li>
+		<li><a href="">Wall<span id="wall"></span></a></li>
+		<li><a href="">My Ratings</a></li>
+	</ul>
+	<h3>Personal Menu</h3>
+	<ul>
+		<li><a href="">Store</a></li>
+		<li><a href="">Control Panel</a></li>
+		<li><a href="">Settings</a></li>
+		<li><a href="">Referral Program</a></li>
+		<li><a href="">Dice Stats</a></li>
+		<li><a href="">Medal Stats</a></li>
+		<li><a href="">Inventory</a></li>
+	</ul>
+</div>
+<!--- left column end -->
+				</div>
+			</div>
+			<div id="middleColumn">
+				<div class="inside">
+<!--- middle (main content) column begin -->
+<?}?>
+<?function HTML_Content_Title() {?>
+<h2>Game Finder</h2>
+<div id="headerline">&nbsp;</div>
+
+<p>Here are all the games that match your search criteria:</p>
+<?}?>
+<?function HTML_Pagination() {?>
+<script type="text/javascript">
+	var totalPages = 9;
+	var currentPage = 5;
+	var gotoUrl = "https://www.conquerclub.com/player.php?submit=Search&order=D&p1=MestreLion&p2=Yellow+Peril";
+</script>
+<div class="ccpagination">
+	<span class="search_results">3 results on 10 pages:</span>
+	<a href="" class="firstLast">&lt;&lt;</a>
+	<a href="" class="nextPrev">&lt;</a>
+	<span class="current_page">Page 5</span>
+	<a href="" class="nextPrev">&gt;</a>
+	<a href="" class="firstLast">&gt;&gt;</a>
+	<a href="#" onclick="togglePagePicker('top'); return false;" class="dropdown">&nbsp;<img src="images/static/pagination_open.gif" alt="v" />&nbsp;</a>
+	<div id="page_picker_top" class="page_picker" style="display:none">
+		<h4 style="text-align:center">Go to page...</h4>
+		<input type="button" value="&lt;" onmousedown="startTicker(-1, 'top');" class="ticker_button" />
+		<input type="text" id="page_picker_input_top" size="4" onkeydown="keyCheck(event, 'top');" />
+		<input type="button" value="&gt;" onmousedown="startTicker(1, 'top');" class="ticker_button" />
+		<input type="button" value="Go" onclick="gotoPage('top');" class="page_picker_button" />
+	</div>
+</div>
+<?}?>
+<?function HTML_Footer() {?>
+<!--- middle (main content) column end -->
+				</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
+
+
+<!-- footer begin -->
+<div id="footer">
+	<table width=100% border=0>
+		<tr height=40>
+			<td align=left valign=center>
+				<div id=footer_nav class=footerNav   style='display: table-cell;height:30px;vertical-align:middle;'>
+					<a href="https://www.conquerclub.com/player.php?mode=mygames1">Conquer Club</a> |
+					<a href="/">Rodrigo Silva</a> |
+					<a href="mailto:conquerclub@rodrigosilva.com">Contact</a>
+				</div>
+			</td>
+			<td width=20></td>
+			<td align=center>
+				<div id=footer_copyright style='width:100%;text-align:center;display: table-cell;height:30px;vertical-align:middle;'>
+					MestreLion's Tools is not associated with Conquer Club or RISK online in any way. Copyright &copy; 2016 by MestreLion
+				</div>
+			</td>
+			<td width=20></td>
+			<td align=right>
+				<div id=footer_social>
+					<a style='padding-left:5px;padding-right:5px;' href='http://facebook.com/rodrigo.es.silva' target=_blank><img border=0 src='images/static/footer-facebookicon.png'></a>
+					<a style='padding-left:5px;padding-right:5px;' href='http://twitter.com/@RodrigoESSilva' target=_blank><img src='images/static/footer-twittericon.png'></a>
+				</div>
+			</td>
+		</tr>
+	</table>
+</div>
+<!-- footer end -->
+
+
+</div>
+</body>
+</html>
+<?}?>
---
+++ web/www/template.php
@@ -0,0 +1,141 @@
+<?include "globals.php"?>
+<?include "includes/lib_common.php"?>
+<?include "includes/lib_html.php"?>
+<?php
+HTML_Header();
+HTML_Content_Title();
+HTML_Pagination();
+HTML_List_Header();
+HTML_List_Item1();
+HTML_List_Item2();
+HTML_List_Item3();
+HTML_List_Footer();
+HTML_Pagination();
+HTML_Footer();
+?>
+<?function HTML_List_Header() {?>
+<table class="listing">
+	<tr>
+		<th>&nbsp;</th>
+		<th>Game Type<br />Initial Troops<br />Play Order</th>
+		<th>Map</th>
+		<th>Spoils<br />Reinforcements<br />Special Gameplay</th>
+		<th>Round Limit<br />Round Length<br />Round</th>
+		<th>Players</th>
+	</tr>
+<?}?>
+<?function HTML_List_Item1() {?>
+	<tr class="odd2">
+		<td colspan="7"><b>Tournament Game: <a href="">2016 [Jan] [CC6] PACK vs FISO</a> - FISO Set 1 - 12 hour fog rule</b></td>
+	</tr>
+	<tr class="odd">
+		<td style='vertical-align:top;' align="center">
+			<span class="gameno">16254463</span>
+			<a class="gameno" href="">Enter Game</a>
+		</td>
+		<td style='vertical-align:top;'>
+			Triples<br />
+			Automatic<br />
+			Sequential
+		</td>
+		<td style='vertical-align:top;'>
+			Jamaica<br />
+			<a href="images/maps/Jamaica3.L.jpg" rel="lightbox" title="Jamaica,75919"><img style="background-image:url(images/maps/Jamaica.thumb.png)" src="images/static/map_normal.png" width="50" height="34" class="thumbnail" alt="Jamaica" title="Jamaica" /></a>
+		</td>
+		<td style='vertical-align:top;'>
+			No Spoils<br />
+			Chained<br />
+			Fog Trench
+		</td>
+		<td style='vertical-align:top;'>
+			None<br />
+			24 Hours<br />
+			Round 11
+		</td>
+		<td style='vertical-align:top;'>
+			<ul class="players">
+				<li><span class="eliminated"><a class="rank p r11" title="Major kizkiz's Profile" href="">kizkiz</a> <a class="rating" title="Major kizkiz's Ratings" href="">4.9</a></span></li>
+				<li><span class="eliminated"><a class="rank p r8" title="Sergeant 1st Class Bobby4254's Profile" href="">Bobby4254</a> <a class="rating" title="Sergeant 1st Class Bobby4254's Ratings" href="">4.9</a></span></li>
+				<li><span class="eliminated"><a class="rank p r10" title="Captain Duke_Spirey's Profile" href="">Duke_Spirey</a> <a class="rating" title="Captain Duke_Spirey's Ratings" href="">4.8</a></span></li>
+				<li><a class="rank p r6" title="Corporal 1st Class MyTurnToWin's Profile" href="">MyTurnToWin</a> <a class="rating" title="Corporal 1st Class MyTurnToWin's Ratings" href="">4.8</a></li>
+				<li><a class="rank p r8" title="Sergeant 1st Class Yellow Peril's Profile" href="">Yellow Peril</a> <a class="rating" title="Sergeant 1st Class Yellow Peril's Ratings" href="">4.6</a></li>
+				<li><a class="rank p r12" title="Colonel MestreLion's Profile" href="">MestreLion</a> <a class="rating" title="Colonel MestreLion's Ratings" href="">4.9</a></li>
+			</ul>
+		</td>
+	</tr>
+<?}?>
+<?function HTML_List_Item2() {?>
+	<tr class="even">
+		<td style='vertical-align:top;' align="center">
+			<span class="gameno">16198006</span>
+			<a class="gameno" href="">Enter Game</a>
+		</td>
+		<td style='vertical-align:top;'>
+			Quadruples<br />
+			Automatic<br />
+			Sequential
+		</td>
+		<td style='vertical-align:top;'>
+			Route 66<br />
+			<a href="images/maps/Route_66.L.jpg" rel="lightbox" title="Route 66,67910"><img style="background-image:url(images/maps/Route_66.thumb.png)" src="images/static/map_normal.png" width="50" height="34" class="thumbnail" alt="Route 66" title="Route 66" /></a>
+		</td>
+		<td style='vertical-align:top;'>
+			No Spoils<br />
+			Chained<br />
+			Fog
+		</td>
+		<td style='vertical-align:top;'>
+			None<br />
+			24 Hours<br />
+			Round 10
+		</td>
+		<td style='vertical-align:top;'>
+			<ul class="players">
+				<li><a class="rank p r11" title="Major Otnal's Profile" href="">Otnal</a> <a class="rating" title="Major Otnal's Ratings" href="">4.8</a></li>
+				<li><a class="rank p r6" title="Corporal 1st Class ScaryTeded's Profile" href="">ScaryTeded</a> <a class="rating" title="Corporal 1st Class ScaryTeded's Ratings" href="">4.8</a></li>
+				<li><a class="rank p r8" title="Sergeant 1st Class Jimmy V's Profile" href="">Jimmy V</a> <a class="rating" title="Sergeant 1st Class Jimmy V's Ratings" href="">4.9</a></li>
+				<li><a class="rank p r12" title="Colonel MestreLion's Profile" href="">MestreLion</a> <a class="rating" title="Colonel MestreLion's Ratings" href="">4.9</a></li>
+				<li><span class="eliminated"><a class="rank p r10" title="Captain BIG_John's Profile" href="">BIG_John</a> <a class="rating" title="Captain BIG_John's Ratings" href="">4.8</a></span></li>
+				<li><span class="eliminated"><a class="rank p r8" title="Sergeant 1st Class Yellow Peril's Profile" href="">Yellow Peril</a> <a class="rating" title="Sergeant 1st Class Yellow Peril's Ratings" href="">4.6</a></span></li>
+				<li><span class="eliminated"><a class="rank p r12" title="Colonel Captn B's Profile" href="">Captn B</a> <a class="rating" title="Colonel Captn B's Ratings" href="">4.9</a></span></li>
+				<li><span class="eliminated"><a class="rank p r11" title="Major dyrtydog's Profile" href="">dyrtydog</a> <a class="rating" title="Major dyrtydog's Ratings" href="">4.6</a></span></li>
+			</ul>
+		</td>
+	</tr>
+<?}?>
+<?function HTML_List_Item3() {?>
+	<tr class="odd">
+		<td style='vertical-align:top;' align="center">
+			<span class="gameno">15435391</span>
+			<a class="gameno" href="">Enter Game</a>
+		</td>
+		<td style='vertical-align:top;'>
+			Standard<br />
+			Automatic<br />
+			Sequential
+		</td>
+		<td style='vertical-align:top;'>
+			Classic<br />
+			<a href="images/maps/Classic.L.jpg" rel="lightbox" title="Classic,97975"><img style="background-image:url(images/maps/Classic2.thumb.png)" src="images/static/map_normal.png" width="50" height="34" class="thumbnail" alt="Classic" title="Classic" /></a>
+		</td>
+		<td style='vertical-align:top;'>
+			Escalating<br />
+			Chained<br />
+			No
+		</td>
+		<td style='vertical-align:top;'>
+			50 Rounds<br />
+			24 Hours<br />
+			Round 9
+		</td>
+		<td style='vertical-align:top;'>
+			<ul class="players">
+				<li><a class="rank p r12" title="Colonel MestreLion's Profile" href="">MestreLion</a> <a class="rating" title="Colonel MestreLion's Ratings" href="">4.9</a></li>
+				<li><span class="eliminated"><a class="rank p r8" title="Sergeant 1st Class Yellow Peril's Profile" href="">Yellow Peril</a> <a class="rating" title="Sergeant 1st Class Yellow Peril's Ratings" href="">4.6</a></span></li>
+			</ul>
+		</td>
+	</tr>
+<?}?>
+<?function HTML_List_Footer() {?>
+</table>
+<?}?>
