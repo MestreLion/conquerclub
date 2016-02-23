e9127d5dde1df094fb739420ba198753484de017
MestreLion <linux@rodrigosilva.com>
2016-02-23 06:00:34 -0300

    read clan data to global PHP var

--- web/www/globals.php
+++ web/www/globals.php
@@ -16,6 +16,28 @@ $APP = array(
 	"MaxAnnouncements"     => 10,
 );
 
+# Clan global array
+$CLAN = array(
+	"name"      => "",
+	"shortname" => "",
+	"clanid"    =>  0,
+	"groupid"   =>  0,
+	"forumid"   =>  0,
+);
+if (($fd = @fopen($APP['DataDir'] . "/clan.txt", "r"))) {
+	$data = fgetcsv($fd, 1000, "\t");
+	if (count($data) == count($CLAN)) {
+		$CLAN["name"]      = trim(  $data[0]);
+		$CLAN["shortname"] = trim(  $data[1]);
+		$CLAN["clanid"]    = intval($data[2]);
+		$CLAN["groupid"]   = intval($data[3]);
+		$CLAN["forumid"]   = intval($data[4]);
+	}
+	unset($data);
+	fclose($fd);
+}
+unset($fd);
+
 # Session vars
 session_start();
 
