33c614f9533cd4ca75171d6b4027faeb5cd79673
MestreLion <linux@rodrigosilva.com>
2016-02-23 06:55:21 -0300

    changet scoreboard CGI to a single table per call

--- web/cgi-bin/league_scoreboard.cgi
+++ web/cgi-bin/league_scoreboard.cgi
@@ -5,15 +5,14 @@ if [[ -f common ]]; then
 fi
 
 htmltableparams=()
+scoreboardparams=()
 
 if [[ "${GET[clan]}" ]]; then
 	htmltableparams+=(-v mark=2,"${GET[clan]}")
 fi
 
-printf "<h4>Finished matches only:</h4>\n"
+if [[ "${GET[mode]}" == "ongoing" ]]; then
+	scoreboardparams+=(--ongoing)
+fi
 
-league_scoreboard | htmltable "${htmltableparams[@]}"
-
-printf "<h4>Including ongoing matches:</h4>\n"
-
-league_scoreboard --ongoing | htmltable "${htmltableparams[@]}"
+league_scoreboard "${scoreboardparams[@]}" | htmltable "${htmltableparams[@]}"
--- web/www/cl7scoreboard.php
+++ web/www/cl7scoreboard.php
@@ -4,6 +4,22 @@
 <?php
 HTML_Header("CL7 Scoreboards");
 HTML_Content_Title("CL7 Scoreboards", "");
-HTML_AnimatedLoadURL("cgi-bin/league_scoreboard.cgi?clan=" . urlencode($CLAN['shortname']));
+
+$scores = array(
+	array("mode" => "finished", "title" => "Finished matches only:"),
+	array("mode" => "ongoing",  "title" => "Including ongoing matches:"),
+);
+foreach($scores as $score) {
+	HTML_Item($score["title"]);
+	HTML_AnimatedLoadURL(
+		"cgi-bin/league_scoreboard.cgi" .
+			"?mode=" . _U($score["mode"]) .
+			"&clan=" . _U($CLAN['shortname']),
+		$score["mode"]
+	);
+}
 HTML_Footer();
 ?>
+<?function HTML_Item($title="") {?>
+	<h4><?=_H($title)?></h4>
+<?}?>
